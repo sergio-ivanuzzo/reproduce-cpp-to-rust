e2a43d989a96f100fbf781e5b2921e73817f9483
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2024-03-26 13:51:47 +0100

    fix

--- src/build.rs
+++ src/build.rs
@@ -1,9 +1,11 @@
 fn main() {
-    let dst = cmake::Config::new("libsrp6").build();
+    let dst = cmake::build("libsrp6");
 
     println!("cargo:rustc-link-search=native={}", dst.display());
-    println!("cargo:rustc-link-lib=dylib=libsrp6");
+    println!("cargo:rustc-link-search=libsrp6");
+    println!("cargo:rustc-link-lib=dylib=srp6");
     println!("cargo:rustc-link-lib=dylib=ssl");
     println!("cargo:rustc-link-lib=dylib=crypto");
     println!("cargo:rustc-link-lib=dylib=stdc++");
 }
+
